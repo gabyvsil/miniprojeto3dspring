8501375b70a209e4064169e8249adcf677ea0fa8
gabyvsil <rm83565@estudaante.fieb.edu.br>
2023-05-23 11:44:24 -0300

    Modificado

--- src/main/java/br/itb/informatica/miniprojeto3dspring/control/ProdutoController.java
+++ src/main/java/br/itb/informatica/miniprojeto3dspring/control/ProdutoController.java
@@ -1,9 +1,16 @@
 package br.itb.informatica.miniprojeto3dspring.control;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.itb.informatica.miniprojeto3dspring.model.Produto;
 import br.itb.informatica.miniprojeto3dspring.service.ProdutoService;
 
 @RestController
@@ -20,5 +27,19 @@ public class ProdutoController {
 		this.produtoService = produtoService;
 	}
 	
+	
+	// Rota POST INSERT INTO PRODUTO
+	@PostMapping
+	public ResponseEntity<Object> saveProduto(Produto produto) {
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(produtoService.save(produto));
+	}
+	
+	// Rota GET Select * from produto
+	@GetMapping
+	public ResponseEntity<List<Produto>> getAllProdutos(){
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(produtoService.findAll());
+	}
 	
 }
